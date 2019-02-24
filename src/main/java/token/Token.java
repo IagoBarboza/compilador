8207dd8c94aade4b98d87396481a29813db804f8
João Correia <jlmc@ic.ufal.br>
2019-02-24 20:32:49 -0300

    Adiciona a enumeração das categorias dos tokens e a classe Token.

--- src/main/java/token/Token.java
+++ src/main/java/token/Token.java
@@ -1,4 +1,16 @@
 package token;
 
 public class Token {
+    public Categoria category;
+    public int line;
+    public int column;
+    public String lexicalValue;
+
+    public Token(Categoria category, int line, int column, String lexicalValue) {
+        this.category = category;
+        this.line = line;
+        this.column = column;
+        this.lexicalValue = lexicalValue;
+    }
+
 }
