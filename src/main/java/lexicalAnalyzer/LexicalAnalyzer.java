e523d7fc81d057c27bfd259d89d797e7da63980a
João Correia <jlmc@ic.ufal.br>
2019-02-25 00:40:06 -0300

    Remove uma categoria de token obsoleta e adiciona uma faltante.

--- src/main/java/lexicalAnalyzer/LexicalAnalyzer.java
+++ src/main/java/lexicalAnalyzer/LexicalAnalyzer.java
@@ -1,8 +1,10 @@
 package lexicalAnalyzer;
 
 import fileManager.FileManager;
+import token.Categoria;
 import token.Token;
 
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,7 +24,7 @@ public class LexicalAnalyzer {
 
         //Lê a primeira linha do arquivo
         String line = fileManager.nextLine();
-
+    findToken(line);
         // Implementar a busca por tokens
         //Retornar um token
         return token;
@@ -34,84 +36,98 @@ public class LexicalAnalyzer {
         String digito = "[0-9]";
         String simbolo = "!|#|\\$|%|&|\\”|\\(|\\)|\\*|\\+|\\,|\\-|\\.|\\/|:|;|<|=|>|\\?|@|[|]|\\^|_|\\‘|\\{|\\||}|\\∼";
 
-        Pattern pMain = Pattern.compile("main");
-        Pattern pVoid = Pattern.compile("void");
-        Pattern pTipInt = Pattern.compile("int");
-        Pattern pTipFlo = Pattern.compile("float");
-        Pattern pTipCha = Pattern.compile("char");
-        Pattern pTipBoo = Pattern.compile("boolean");
-        Pattern pTipStr = Pattern.compile("string");
-        Pattern pReturn = Pattern.compile("return");
-        Pattern pIf = Pattern.compile("if");
-        Pattern pElse = Pattern.compile("else");
-        Pattern pFor = Pattern.compile("for");
-        Pattern pIn = Pattern.compile("in");
-        Pattern pTo = Pattern.compile("to");
-        Pattern pStep = Pattern.compile("step");
-        Pattern pWhile = Pattern.compile("while");
-        Pattern pVar = Pattern.compile("var");
-        Pattern pInput = Pattern.compile("input");
-        Pattern oOutput = Pattern.compile("output");
-        Pattern pFun = Pattern.compile("fun");
-        Pattern pContinue = Pattern.compile("continue");
-        Pattern pBreak = Pattern.compile("break");
+        Map<Pattern, Categoria> pattern = new LinkedHashMap<Pattern, Categoria>();
+
+        pattern.put(Pattern.compile("main"), Categoria.MAIN);
+        pattern.put(Pattern.compile("void"), Categoria.VOID);
+        pattern.put(Pattern.compile("int"), Categoria.TIP_INT);
+        pattern.put(Pattern.compile("float"), Categoria.TIP_FLO);
+        pattern.put(Pattern.compile("char"), Categoria.TIP_CHA);
+        pattern.put(Pattern.compile("boolean"), Categoria.TIP_BOO);
+        pattern.put(Pattern.compile("string"), Categoria.TIP_STR);
+        pattern.put(Pattern.compile("return"), Categoria.RETURN);
+        pattern.put(Pattern.compile("if"), Categoria.IF);
+        pattern.put(Pattern.compile("else"), Categoria.ELSE);
+        pattern.put(Pattern.compile("for"), Categoria.FOR);
+        pattern.put(Pattern.compile("in"), Categoria.IN);
+        pattern.put(Pattern.compile("to"), Categoria.TO);
+        pattern.put(Pattern.compile("step"), Categoria.STEP);
+        pattern.put(Pattern.compile("while"), Categoria.WHILE);
+        pattern.put(Pattern.compile("var"), Categoria.VAR);
+        pattern.put(Pattern.compile("read"), Categoria.INPUT);
+        pattern.put(Pattern.compile("print"), Categoria.OUTPUT);
+        pattern.put(Pattern.compile("fun"), Categoria.FUN);
+        pattern.put(Pattern.compile("continue"), Categoria.CONTINUE);
+        pattern.put(Pattern.compile("break"), Categoria.BREAK);
+
+        pattern.put(Pattern.compile(digito+"+"),
+                Categoria.CON_INT);
+        pattern.put(Pattern.compile("\\["+digito+"(,"+digito+"+)*\\]"),
+                Categoria.CON_INT_CAD);
+        pattern.put(Pattern.compile(digito+"+\\."+digito+"+"),
+                Categoria.CON_FLO);
+        pattern.put(Pattern.compile("\\["+digito+"+\\."+digito+"+(,"+digito+"+\\."+digito+"+)*\\]"),
+                Categoria.CON_FLO_CAD);
+        pattern.put(Pattern.compile("true|false"),
+                Categoria.CON_BOO);
+        pattern.put(Pattern.compile("\\[true|false(,(true|false))*\\]"),
+                Categoria.CON_BOO_CAD);
+        pattern.put(Pattern.compile("'("+ letra + "|" + digito + "|" + simbolo + ")'"),
+                Categoria.CON_CHA);
+        pattern.put(Pattern.compile("\\['("+ letra + "|" + digito + "|" + simbolo + ")'"+
+                        "(,'("+ letra + "|" + digito + "|" + simbolo + ")')*\\]"),
+                Categoria.CON_CHA_CAD);
+        pattern.put(Pattern.compile("\"("+ letra + "|" + digito + "|" + simbolo + ")*\""),
+                Categoria.CON_STR);
+        pattern.put(Pattern.compile("\\[\"("+ letra + "|" + digito + "|" + simbolo + ")*\"" +
+                        "(,\"("+ letra + "|" + digito + "|" + simbolo + ")*\")*\\]"),
+                Categoria.CON_STR_CAD);
+
+        pattern.put(Pattern.compile("\\)"), Categoria.FEC_PAR);
+        pattern.put(Pattern.compile("\\("), Categoria.ABR_PAR);
+        pattern.put(Pattern.compile("\\}"), Categoria.FEC_CHA);
+        pattern.put(Pattern.compile("\\{"), Categoria.ABR_CHA);
+        pattern.put(Pattern.compile("\\["), Categoria.ABR_COL);
+        pattern.put(Pattern.compile("\\]"), Categoria.FEC_COL);
+        pattern.put(Pattern.compile("\\+"), Categoria.OPE_ADI);
+        pattern.put(Pattern.compile("-"), Categoria.OPE_SUB);
+        pattern.put(Pattern.compile("\\*"), Categoria.OPE_MUL);
+        pattern.put(Pattern.compile("\\/"), Categoria.OPE_DIV);
+        pattern.put(Pattern.compile("=="), Categoria.OPE_IGU);
+        pattern.put(Pattern.compile("!="), Categoria.OPE_DIF);
+        pattern.put(Pattern.compile("<"), Categoria.OPE_MEN);
+        pattern.put(Pattern.compile(">"), Categoria.OPE_MAI);
+        pattern.put(Pattern.compile("<="), Categoria.OPE_MEN_IGU);
+        pattern.put(Pattern.compile(">="), Categoria.OPE_MAI_IGU);
+        pattern.put(Pattern.compile("\\|\\|"), Categoria.OPE_OU);
+        pattern.put(Pattern.compile("&&"), Categoria.OPE_E);
+        pattern.put(Pattern.compile("!"), Categoria.OPE_NEG);
+        pattern.put(Pattern.compile("\\+\\+"), Categoria.OPE_CON);
+        pattern.put(Pattern.compile(","), Categoria.VIRGULA);
+        pattern.put(Pattern.compile(";"), Categoria.PON_VIR);
+        pattern.put(Pattern.compile("="), Categoria.ATRIBUICAO);
+
+        pattern.put(Pattern.compile(letra + "(" + letra + "|" + digito +")*"),
+                Categoria.IDENTIFICADOR);
 
 
-        Pattern pFecPar = Pattern.compile("\\)");
-        Pattern pAbrPar = Pattern.compile("\\(");
-        Pattern pFecCha = Pattern.compile("\\}");
-        Pattern pAbrCha = Pattern.compile("\\{");
-        Pattern pAbrCol = Pattern.compile("\\[");
-        Pattern pFecCol = Pattern.compile("\\]");
-        Pattern pOpeAdi = Pattern.compile("\\+");
-        Pattern pOpeSubNeg = Pattern.compile("-");
-        Pattern pOpeMul = Pattern.compile("\\*");
-        Pattern pOpeDiv = Pattern.compile("\\/");
-        Pattern pOpeIgu = Pattern.compile("==");
-        Pattern pOpeDif = Pattern.compile("!=");
-        Pattern pOpeMen = Pattern.compile("<");
-        Pattern pOpeMai = Pattern.compile(">");
-        Pattern pOpeMenIgu = Pattern.compile("<=");
-        Pattern pOpeMaiIgu = Pattern.compile(">=");
-        Pattern pOpeOu = Pattern.compile("||");
-        Pattern pOpeE = Pattern.compile("&&");
-        Pattern pOpeNeg = Pattern.compile("!");
-        Pattern pOpeCon = Pattern.compile("\\+\\+");
-        Pattern pVir = Pattern.compile(",");
-        Pattern pPonVir = Pattern.compile(";");
-        Pattern pAtr = Pattern.compile("=");
+        //Remove spaces at the begin of line, and calculates how much spaces were removed
+        String noSpacesAtBeginLine = line.replaceFirst("\\s*", "");
+        int removedSpacesAtBegin = line.length() - noSpacesAtBeginLine.length();
 
-        Pattern pIde = Pattern.compile(letra + "(" + letra + "|" + digito +")*");
+        for (Map.Entry<Pattern, Categoria> element : pattern.entrySet()) {
 
-        Pattern pConInt = Pattern.compile(digito+"+");
-        Pattern pConIntCad = Pattern.compile("\\["+digito+"(,"+digito+"+)*\\]");
+            Matcher matcher = element.getKey().matcher(noSpacesAtBeginLine);
 
-        Pattern pConFlo = Pattern.compile(digito+"+\\."+digito+"+");
-        Pattern pConFloCad= Pattern.compile("\\["+digito+"+\\."+digito+"+(,"+digito+"+\\."+digito+"+)*\\]");
-
-        Pattern pConBoo = Pattern.compile("true|false");
-        Pattern pConBooCad= Pattern.compile("\\[true|false(,(true|false))*\\]");
-
-        Pattern pConCha = Pattern.compile("'("+ letra + "|" + digito + "|" + simbolo + ")'");
-        Pattern pConChaCad= Pattern.compile("\\['("+ letra + "|" + digito + "|" + simbolo + ")'"+
-                                             "(,'("+ letra + "|" + digito + "|" + simbolo + ")')*\\]");
-
-        Pattern pConStr = Pattern.compile("\"("+ letra + "|" + digito + "|" + simbolo + ")*\"");
-        Pattern pConStrCad= Pattern.compile("\\[\"("+ letra + "|" + digito + "|" + simbolo + ")*\"" +
-                                             "(,\"("+ letra + "|" + digito + "|" + simbolo + ")*\")*\\]");
-
-        //Implementar a definição de todos os metchers
-        Matcher mMain = pMain.matcher(line);
-        Matcher mVoid = pVoid.matcher(line);
-
-        //Busca o próximo token na linha recebida
-        Token token = null;
-        if(mMain.find()) {
-
-        }else if(mVoid.find()){
-
+            if(matcher.find()){
+                if(matcher.start() == 0) {
+                    System.out.println(element.getValue().toString());
+                    return null;
+                }
+            }
         }
 
+        Token token = null;
         return token;
     }
 
