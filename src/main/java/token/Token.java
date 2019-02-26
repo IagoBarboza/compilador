513250079a289758d3619bd3abfea02024443c09
Myron <myrondavid96@gmail.com>
2019-02-25 23:06:03 -0300

    Correções na classe Token.

--- src/main/java/token/Token.java
+++ src/main/java/token/Token.java
@@ -1,16 +1,52 @@
 package token;
 
 public class Token {
-    public Categoria category;
-    public int line;
-    public int column;
-    public String lexicalValue;
+    private Category category;
+    private int line;
+    private int column;
+    private String lexicalValue;
 
-    public Token(Categoria category, int line, int column, String lexicalValue) {
+    public Token(Category category, int line, int column, String lexicalValue) {
         this.category = category;
         this.line = line;
         this.column = column;
         this.lexicalValue = lexicalValue;
     }
 
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public String getLexicalValue() {
+        return lexicalValue;
+    }
+
+    public void setLexicalValue(String lexicalValue) {
+        this.lexicalValue = lexicalValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" + "category=" + category + ", line=" + line + ", column=" + column + ", lexicalValue='" + lexicalValue + '\'' + '}';
+    }
 }
